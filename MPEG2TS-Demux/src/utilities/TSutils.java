6d1ceea21914f80499d1d36bac43e9d8bf0d8709
elias3@gmail.com <elias3@gmail.com@82fcc815-9f44-3e94-5ac8-23f0d3c1cf48>
2010-03-19 11:48:53 +0000

    Project created.

---
+++ MPEG2TS-Demux/src/utilities/TSutils.java
@@ -0,0 +1,106 @@
+
+package utilities;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class TSutils {
+
+	
+	
+
+	public static boolean isAdaptationFieldExist(byte[] tsPacket) {
+		return ((tsPacket[3] & 0x20) != 0);
+	}
+	
+	
+	public static boolean isPCRExist(byte[] tsPacket) {
+		int offset = 5;
+
+		if (isAdaptationFieldExist(tsPacket) && (tsPacket[offset] & 0x10) != 0)
+			return true;
+
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param tsPacket
+	 * @return adaptation field length, assumes that adaptation field exists
+	 */
+	public static int adaptationFieldLength(byte[] tsPacket) {
+		return (tsPacket[4] & 0xff);
+	}
+	
+
+	public static int tsDataOffset(byte[] tsPacket) {
+		int offset = 4;
+		
+		if (isAdaptationFieldExist(tsPacket))
+			offset += 1 + adaptationFieldLength(tsPacket);
+		
+		return offset;
+	}
+
+
+	public static boolean isStartOfPES(byte[] tsPacket) {
+		if ((tsPacket[1] & 0x40) == 0)
+			return false;
+
+		int offset = tsDataOffset(tsPacket);
+		
+		return (tsPacket[offset] == 0 && tsPacket[offset+1] == 0 && tsPacket[offset+2] == 0x01);
+	}
+
+
+	public static long getPTS(byte[] tsPacket) {
+		
+		int offset = tsDataOffset(tsPacket);
+
+		offset += 9; // offset now points to the MSB(byte) of the PTS.
+		
+		long pts = (tsPacket[offset] & 0x0e) >> 1;		
+		pts      = (pts << 15) + ((tsPacket[offset+1] & 0xff) << 7) + ((tsPacket[offset+2] & 0xfe) >> 1);
+		pts      = (pts << 15) + ((tsPacket[offset+3] & 0xff) << 7) + ((tsPacket[offset+4] & 0xfe) >> 1);
+		
+		return pts;
+	}
+
+
+
+	
+	public static long getDTS(byte[] tsPacket) {
+		
+		int offset = tsDataOffset(tsPacket);
+
+		offset += 14; // offset now points to the MSB(byte) of the DTS.
+		
+		long dts = (tsPacket[offset] & 0x0e) >> 1;		
+		dts      = (dts << 15) + ((tsPacket[offset+1] & 0xff) << 7) + ((tsPacket[offset+2] & 0xfe) >> 1);
+		dts      = (dts << 15) + ((tsPacket[offset+3] & 0xff) << 7) + ((tsPacket[offset+4] & 0xfe) >> 1);
+		
+		return dts;
+	}
+
+
+	
+	
+	
+	public static long getPCRbase(byte[] tsPacket) {
+		int offset = 6;
+		
+		return    ((tsPacket[offset]   & 0xff) << 25)
+				+ ((tsPacket[offset+1] & 0xff) << 17)
+				+ ((tsPacket[offset+2] & 0xff) <<  9)
+				+ ((tsPacket[offset+3] & 0xff) <<  1)
+				+ ((tsPacket[offset+4] & 0x80) >>  7);				
+	}
+
+	
+	
+
+	
+}
