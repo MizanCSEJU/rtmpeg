9a106135ac31590896ebd5b57bb3c3f4af7e60e8
elias3@gmail.com <elias3@gmail.com@82fcc815-9f44-3e94-5ac8-23f0d3c1cf48>
2010-03-27 17:03:02 +0000

    Added implementation of adding packets from PES start to PES start. Need to implement the getPayload method.

--- MPEG2TS-Demux/src/demux/Demultiplexer.java
+++ MPEG2TS-Demux/src/demux/Demultiplexer.java
@@ -2,15 +2,16 @@ package demux;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import utilities.TSutils;
 
 /**
  * 
- * A demultiplexer of MPEG2-TS file, the aim of this class is to provide frames
+ * A de-multiplexer of MPEG2-TS file, the aim of this class is to provide frames
  * from an MPEG2-TS file that is in a H264 format.
  * 
  * @author Elias Khsheibun
@@ -21,11 +22,34 @@ public class Demultiplexer {
 
 	private final static int packetSize = 188;
 	private final static int pidH264 = 68;
-	private static int frameNum = 0;
+	private static int packetNum = 0;
+	private int packetBufferNum = 0;
+	private final static int bufferSize = 10;
+	private byte[][] buffer = new byte[bufferSize][packetSize];
+	private int bufferPointer;
 	private InputStream is;
+	private long noOfPacketsInFile;
+	private long frameNo;
+	private long offset;
+	private long timeStamp;
 
-	public Demultiplexer(File file) throws FileNotFoundException {
+	private void fillBuffer() throws IOException {
+		for (int i = 0; i < buffer.length && packetNum + 1 < noOfPacketsInFile; i++) {
+			buffer[i] = getNextTSPacket();
+			packetBufferNum++;
+		}
+
+		bufferPointer = 0;
+	}
+
+	public Demultiplexer(File file) throws IOException {
 		is = new FileInputStream(file);
+		noOfPacketsInFile = (file.length() / packetSize);
+		fillBuffer();
+		bufferPointer = 0;
+		frameNo = 0;
+		offset = 0;
+		timeStamp = 0;
 	}
 
 	private byte[] getNextTSPacket() throws IOException {
@@ -35,27 +59,101 @@ public class Demultiplexer {
 			System.err.println("End of stream has been reached");
 		}
 
-		frameNum++;
 		return b;
 	}
 
-	public Frame getNextFrame() {
-		return null;
+	public Frame getNextFrame() throws IOException {
+
+		List<byte[]> arrayList = new ArrayList<byte[]>();
+
+		if (packetNum >= noOfPacketsInFile)
+			return null;
+
+		// The first loop for finding the start of a PES
+		for (; packetNum < noOfPacketsInFile; packetNum++, bufferPointer++) {
+
+			if (bufferPointer == buffer.length)
+				fillBuffer();
+
+			if (TSutils.getPID(buffer[bufferPointer]) != pidH264) {
+				continue;
+			}
+
+			if (TSutils.isStartOfPES(buffer[bufferPointer])) {
+				byte[] payload = getPayload(buffer[bufferPointer]);
+				arrayList.add(payload);
+				packetNum++;
+				bufferPointer++;
+				break;
+			}
+		}
+
+		// Adding packet payloads until next PES is found
+		for (; packetNum < noOfPacketsInFile; packetNum++, bufferPointer++) {
+			if (bufferPointer == buffer.length)
+				fillBuffer();
+
+			if (TSutils.getPID(buffer[bufferPointer]) != pidH264) {
+				continue;
+			}
+
+			if (!TSutils.isStartOfPES(buffer[bufferPointer])) {
+				byte[] payload = getPayload(buffer[bufferPointer]);
+				arrayList.add(payload);
+			} else {
+				break;
+			}
+		}
+
+		if (packetNum > noOfPacketsInFile)
+			return null;
+
+		int totalSize = 0;
+		for (byte[] b : arrayList) {
+			totalSize += b.length;
+		}
+
+		byte[] frame = new byte[totalSize];
+		int j = 0;
+		for (byte[] b : arrayList) {
+			for (int i = 0; i < b.length; i++)
+				frame[j] = b[i];
+			j += b.length;
+		}
+		Frame f = new Frame(frame, offset, frame.length, timeStamp);
+		offset += frame.length;
+		timeStamp++;
+		frameNo++;
+		return f;
+	}
+
+	private byte[] getPayload(byte[] bs) {
+		// TODO Auto-generated method stub
+		return new byte[100];
 	}
 
 	public static void main(String[] args) throws IOException {
 		File file = new File("video.mpg");
 		Demultiplexer demux = new Demultiplexer(file);
-		byte b[] = null;
-		int counter = 0;
-		System.out.println(file.length());
+		int counter2 = 0;
+		while (demux.getNextFrame() != null)
+			counter2++;
+		System.out.println("No of PES packets produced "+counter2);
 
+		int counter = 0;
+		int pesCounter = 0;
+		System.out.println("The length of the file in bytes: "+file.length());
+		demux = new Demultiplexer(file);
+		byte [] b=null;
 		for (int i = 0; i < file.length() / packetSize; i++) {
 			b = demux.getNextTSPacket();
+			if (TSutils.isStartOfPES(b) && TSutils.getPID(b) == pidH264)
+				pesCounter++;
 			if (TSutils.getPID(b) == pidH264)
 				counter++;
 		}
-		System.out.println("No of frames read: " + frameNum);
+		System.out.println("No of PES packets: " + pesCounter);
+		System.out.println("No of frames read: " + packetNum);
 		System.out.println("No of frames with PID = " + pidH264 + " is: "
 				+ counter);
 	}
