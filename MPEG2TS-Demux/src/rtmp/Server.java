1a9ef4f51ca0efa767062c8f643ecdeeba2847ba
elias3@gmail.com <elias3@gmail.com@82fcc815-9f44-3e94-5ac8-23f0d3c1cf48>
2010-04-18 22:00:11 +0000

    handshake implemented (without a proof that's its right).

--- MPEG2TS-Demux/src/rtmp/Server.java
+++ MPEG2TS-Demux/src/rtmp/Server.java
@@ -34,44 +34,53 @@ public class Server {
 		BufferedReader in = new BufferedReader(new InputStreamReader(
 				clientSocket.getInputStream()));
 
-		int s0 = in.read();
+		int c0 = in.read();
 		System.out.println("C0 read");
+		byte s0 = 3;
 		out.write(s0);
 		System.out.println("S0 was sent");
 		Random randomizer = new Random();
 
-		char[] c1 = new char[1536];
+		char[] c1 = new char[768];
 		in.read(c1);
-
+		
 		System.out.println("C1 read");
 
-		char[] s1 = new char[1536];
-		for (int i = 0; i < 4; i++)
+		
+		char[] s1 = new char[768];
+		
+		for (int i = 0; i < 2; i++)
 			s1[i] = c1[i];
-		for (int i = 4; i < 8; i++)
+		for (int i = 2; i < 4; i++)
 			s1[i] = 0;
-		for (int i = 8; i < 1536; i++)
+		for (int i=4 ; i<768 ; i++)
 			s1[i] = (char) randomizer.nextInt();
+		
 
-		out.write(s1);
+		for (int i = 0; i < s1.length; i++)
+			out.write(s1[i]);
 		System.out.println("S1 was sent");
-
-		char[] c2 = new char[1536];
+	
+		char[] c2 = new char[768];
 		in.read(c2);
 		System.out.println("C2 read");
 
-		char[] s2 = new char[1536];
+		char[] s2 = new char[768];
 
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < 2; i++)
 			s2[i] = c1[i];
-		for (int i = 4; i < 8; i++)
+		for (int i = 2; i < 4; i++)
 			s2[i] = s1[i];
-		for (int i = 8; i < 1536; i++)
+		for (int i = 4; i < 768; i++)
 			s2[i] = c1[i];
 
 		out.write(s2);
 
 		System.out.println("S2 was sent");
+		
+		for (int i=0 ;i<100 ; i++){
+			System.out.println("No:"+i+" Data: "+in.read());
+		}
 
 		out.close();
 		in.close();
