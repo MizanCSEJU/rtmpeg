ca1265ec35871713d013e7032b7c42ec18639539
elias3@gmail.com <elias3@gmail.com@82fcc815-9f44-3e94-5ac8-23f0d3c1cf48>
2010-04-18 20:08:28 +0000

    handshake prototype

--- MPEG2TS-Demux/src/rtmp/Server.java
+++ MPEG2TS-Demux/src/rtmp/Server.java
@@ -10,83 +10,78 @@ import java.net.UnknownHostException;
 import java.util.Random;
 
 public class Server {
-	
-	void handShake() throws UnknownHostException, IOException{
+
+	void handShake() throws UnknownHostException, IOException {
 		ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(5080);
-        } catch (IOException e) {
-            System.err.println("Could not listen on port: 5080.");
-            System.exit(1);
-        }
+		try {
+			serverSocket = new ServerSocket(5080);
+		} catch (IOException e) {
+			System.err.println("Could not listen on port: 5080.");
+			System.exit(1);
+		}
 
-        Socket clientSocket = null;
-        try {
-        	System.out.println("Listening...");
-            clientSocket = serverSocket.accept();
-            System.out.println("Connection made !");
-        } catch (IOException e) {
-            System.err.println("Accept failed.");
-            System.exit(1);
-        }
+		Socket clientSocket = null;
+		try {
+			System.out.println("Listening...");
+			clientSocket = serverSocket.accept();
+			System.out.println("Connection made !");
+		} catch (IOException e) {
+			System.err.println("Accept failed.");
+			System.exit(1);
+		}
 
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(
-				new InputStreamReader(
+		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+		BufferedReader in = new BufferedReader(new InputStreamReader(
 				clientSocket.getInputStream()));
-        String inputLine, outputLine;
-        
-        int s0 = in.read();
-        System.out.println("C0 read");
-        out.write(s0);
-        System.out.println("S0 was sent");
-        Random randomizer = new Random();
-        
-        char[] c1 = new char[1536];
-        in.read(c1);
-        
-        System.out.println("C1 read");
 
-        
-        char[] s1 = new char[1536];
-        for (int i=0; i<4 ; i++)
-        	s1[i]=c1[i];
-        for (int i=4; i<8 ; i++)
-        	s1[i]=0;
-        for (int i=8; i<1536 ; i++)
-            s1[i] = (char) randomizer.nextInt();
-        
-        out.write(s1);
-        System.out.println("S1 was sent");
-        
-        char[] c2 = new char[1536];
-        in.read(c2);
-        System.out.println("C2 read");
-        
-        char[] s2 = new char[1536];
-        
-        for (int i=0; i<4 ; i++)
-        	s2[i]=c1[i];
-        for (int i=4; i<8 ; i++)
-        	s2[i]=s1[i];
-        for (int i=8; i<1536 ; i++)
-        	s2[i]=c1[i];
-        
-        out.write(s2);
-        
-        System.out.println("S2 was sent");
-        
-        
-        out.close();
-        in.close();
-        clientSocket.close();
-        serverSocket.close();
-    }
+		int s0 = in.read();
+		System.out.println("C0 read");
+		out.write(s0);
+		System.out.println("S0 was sent");
+		Random randomizer = new Random();
 
-		
-	
-	public static void main(String args[]) throws UnknownHostException, IOException{
+		char[] c1 = new char[1536];
+		in.read(c1);
+
+		System.out.println("C1 read");
+
+		char[] s1 = new char[1536];
+		for (int i = 0; i < 4; i++)
+			s1[i] = c1[i];
+		for (int i = 4; i < 8; i++)
+			s1[i] = 0;
+		for (int i = 8; i < 1536; i++)
+			s1[i] = (char) randomizer.nextInt();
+
+		out.write(s1);
+		System.out.println("S1 was sent");
+
+		char[] c2 = new char[1536];
+		in.read(c2);
+		System.out.println("C2 read");
+
+		char[] s2 = new char[1536];
+
+		for (int i = 0; i < 4; i++)
+			s2[i] = c1[i];
+		for (int i = 4; i < 8; i++)
+			s2[i] = s1[i];
+		for (int i = 8; i < 1536; i++)
+			s2[i] = c1[i];
+
+		out.write(s2);
+
+		System.out.println("S2 was sent");
+
+		out.close();
+		in.close();
+		clientSocket.close();
+		serverSocket.close();
+	}
+
+	public static void main(String args[]) throws UnknownHostException,
+			IOException {
 		new Server().handShake();
 	}
-	
+
 }
