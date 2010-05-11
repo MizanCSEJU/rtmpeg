ba3c4671389e390fcadd9587439e23b57a6b994f
rashed.r87@gmail.com <rashed.r87@gmail.com@82fcc815-9f44-3e94-5ac8-23f0d3c1cf48>
2010-05-11 21:20:35 +0000

    vlc attempt to send video.

--- MPEG2TS-Demux/src/rtmp/Server.java
+++ MPEG2TS-Demux/src/rtmp/Server.java
@@ -31,7 +31,6 @@ public class Server {
 		}
 		OutputStream out = clientSocket.getOutputStream();
 		InputStream in = clientSocket.getInputStream();
-		
 
 		int c0 = in.read();
 		System.out.println("C0 read");
@@ -40,139 +39,71 @@ public class Server {
 		byte[] c1 = new byte[in.available()];
 		in.read(c1);
 
-		System.out.println("C1 read: "+c1.length+" bytes.");
-		out.write(Utils.readFile("handshake.dat"));
+		System.out.println("C1 read: " + c1.length + " bytes.");
+		out.write(Utils.readFile("vlc_hs2"));
 		Utils.waitForStream(in);
-		
+
 		byte[] c2 = new byte[in.available()];
-		System.out.println("No of bytes in c2:"+in.read(c2));
+		System.out.println("No of bytes in c2:" + in.read(c2));
 
 		System.out.println("C2 read");
 		Utils.printStream(c2);
-		
-		out.write(Utils.readFile("invoke_1.dat"));
-		out.write(Utils.readFile("server_bw_1.dat"));
-		out.write(Utils.readFile("ping_1.dat"));
-		out.write(Utils.readFile("invoke_2.dat"));
 
-		
+		out.write(Utils.readFile("vlc_invoke1_done"));
+		out.write(Utils.readFile("vlc_serverbw"));
+		out.write(Utils.readFile("vlc_ping"));
+		out.write(Utils.readFile("vlc_invoke2"));
+
 		System.out.println("\nwaiting 1...");
-		
+
 		Utils.waitForStream(in);
-		
+
 		byte[] arr = new byte[in.available()];
 		in.read(arr);
 
-		
 		System.out.println("\nClient response to bwPingInvoke: ");
 		Utils.printStream(arr);
-		out.write(Utils.readFile("invoke_3.dat"));
-		out.write(Utils.readFile("invoke_1.dat"));
+		String response = new String(arr);
+		if (response.indexOf("play") > 0) {
+			System.out.println("A\n");
+			out.write(Utils.readFile("vlc_invoke3"));
+			out.write(Utils.readFile("vlc_ping2"));
+			out.write(Utils.readFile("vlc_ping2"));
+			out.write(Utils.readFile("vlc_invoke4"));
+			out.write(Utils.readFile("vlc_invoke5"));
+			out.write(Utils.readFile("vlc_notify"));
+		} else {
+			System.out.println("\nwaiting 2...");
 
-		//out.write(Utils.readFile("server_bw_1.dat"));
-	//	out.write(Utils.readFile("ping_1.dat"));
+			Utils.waitForStream(in);
 
-	//	out.write(Utils.readFile("invoke_2.dat"));
-		System.out.println("\nwaiting 2...");
-		
-		Utils.waitForStream(in);
-		
-		arr = new byte[in.available()];
-		in.read(arr);
+			arr = new byte[in.available()];
+			in.read(arr);
 
-		System.out.println("\nClient response to second invoke: ");
-		Utils.printStream(arr);
-		
-		out.write(Utils.readFile("invoke_3.dat"));
-	//	out.write(Utils.readFile("notify_1"));
-		//out.write(Utils.readFile("ping_2"));
-	//	out.write(Utils.readFile("ping_2"));
-	//	out.write(Utils.readFile("invoke_5"));
-		//out.write(Utils.readFile("video_1"));
-	
-
+			System.out.println("\nClient response to second invoke: ");
+			Utils.printStream(arr);
+			System.out.println("B\n");
+			out.write(Utils.readFile("vlc_invoke3"));
+			out.write(Utils.readFile("vlc_ping2"));
+			out.write(Utils.readFile("vlc_ping2"));
+			out.write(Utils.readFile("vlc_invoke4"));
+			out.write(Utils.readFile("vlc_invoke5"));
+			out.write(Utils.readFile("vlc_notify"));
+		}
 
 		System.out.println("\nwaiting 3...");
-		
+
+		for (int i = 1; i <= 100; i++)
+			out.write(Utils.readFile(Integer.toString(i)));
+
 		Utils.waitForStream(in);
-		
+
 		arr = new byte[in.available()];
 		in.read(arr);
 
 		System.out.println("\nClient response to third invoke: ");
 		Utils.printStream(arr);
 
-
-		
-/*
-		System.out.println("\nwaiting 2...");
-		while (in.available() == 0) {
-		}
-
-		System.out.println("\nAvailable: " + in.available());
-		arr = new byte[in.available()];
-
-		in.read(arr);
-		System.out.println("\nCreate Stream command: ");
-		System.out.println(new String(arr));
-
-		// Should now send Command Message (_result - createStream response)
-		// page 25 in RTMP Commands Messages
-		//out.write(invokePayload);
-		
-		f = new File("cs_res.dat");
-		byte[] createStreamResponsePayload = new byte[(int) f.length()];
-		InputStream csResponsePayload = new FileInputStream(f);
-		csResponsePayload.read(createStreamResponsePayload);
-		
-		out.write(createStreamResponsePayload);
-		System.out.println("\nwaiting...");
-		while (in.available() == 0) {
-		}
-
-		System.out.println("\nAvailable: " + in.available());
-		arr = new byte[in.available()];
-		in.read(arr);
-
-		System.out.println("\nPlay command: \n"+new String(arr));
-		for (int i = 0; i < arr.length; i++) {
-			Integer h = ((arr[i] & 0xf0) >> 4);
-			Integer l = (arr[i] & 0x0f);
-			System.out.print(Integer.toHexString(h) + Integer.toHexString(l));
-		}
-		
-		
-		byte [] serverBw = {0x02,0x0,0x0,0x0,0x0,0x0,0x04,0x05,0x0,0x0,0x0,0x0,0x0,0x2,0x0,0x0};
-		byte [] ping = {0x42,0x0,0x0,0x0,0x0,0x0,0x06,0x04,0x0,0x0,0x0,0x0,0x0,0x0};
-		System.out.println("\nPing: "+new String(ping));
-		f = new File("invoke2.dat");
-		byte[] invoke = new byte[(int) f.length()];
-		InputStream is = new FileInputStream(f);
-		
-		is.read(invoke);
-		out.write(serverBw);
-		out.write(ping);
-		out.write(invoke);
-		f = new File("invoke4.dat");
-		invoke = new byte[(int) f.length()];
-		is.read(invoke);
-		out.write(invoke);
-		
-		
-		System.out.println("\nwaiting...");
-		while (in.available() == 0) {
-		}
-		System.out.println("\nAvailable: " + in.available());
-		arr = new byte[in.available()];
-		in.read(arr);
-		System.out.println("\nClient reply to serverBw: \n"+new String(arr));
-		for (int i = 0; i < arr.length; i++) {
-			Integer h = ((arr[i] & 0xf0) >> 4);
-			Integer l = (arr[i] & 0x0f);
-			System.out.print(Integer.toHexString(h) + Integer.toHexString(l));
-		}
-		
-		*/
 		out.close();
 		in.close();
 		clientSocket.close();
